465783e75611c48c444ad44e884773b49e7a6394
Siim Marten Schmidt <siimmarten.schmidt@proton.me>
2026-01-11 16:35:55 +0200

    Make the chime work every hour, added toggle functionality

--- app/src/main/java/ee/fosschime/MainActivity.kt
+++ app/src/main/java/ee/fosschime/MainActivity.kt
@@ -1,9 +1,18 @@
 package ee.fosschime
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -33,18 +42,28 @@ import androidx.compose.ui.unit.dp
 import ee.fosschime.ui.theme.FosschimeTheme
 
 import ee.fosschime.composables.AppHeader
+import java.util.Calendar
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             FosschimeTheme {
 
+                var isChimeOn by remember {mutableStateOf(true)}
+                var overrideSilent by remember {mutableStateOf(false)}
+
                 Column {
                     AppHeader(titleResId = R.string.app_name) { }
-                    OnOffToggle("Hourly chime") { }
-                    OnOffToggle("Override silent mode") { }
+                    OnOffToggle("Hourly chime", isChimeOn) { newValue ->
+                        isChimeOn = newValue
+                        alarmEveryHour(isChimeOn, overrideSilent)
+                    }
+                    OnOffToggle("Override silent mode", overrideSilent) { newValue ->
+                        overrideSilent = newValue
+                    }
                 }
             }
 
@@ -53,45 +72,63 @@ class MainActivity : ComponentActivity() {
 
 
 
-@Composable
-fun OnOffToggle(toggleDescription: String, toggleFunction: () -> Unit){
-    val shape = RoundedCornerShape(32.dp)
-    Row(modifier = Modifier
-        .padding(40.dp,20.dp,40.dp,20.dp)
-        .clip(shape)
-        .background(color = Color.LightGray)
-        .fillMaxWidth()
-        .padding(10.dp, 0.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically)
+    @Composable
+    fun OnOffToggle(description: String, checked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+        val shape = RoundedCornerShape(32.dp)
+        Row(
+            modifier = Modifier
+                .padding(40.dp, 20.dp)
+                .clip(shape)
+                .background(color = Color.LightGray)
+                .fillMaxWidth()
+                .padding(10.dp, 0.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        )
         {
 
-        Text(
-            text = toggleDescription,
-            textAlign = TextAlign.Center
+            Text(
+                text = description,
+                textAlign = TextAlign.Center
 
-        )
-        var checked by remember { mutableStateOf(true) }
+            )
 
-        Switch(
-            checked = checked,
-            onCheckedChange = {
-                checked = it
-            },
-            thumbContent = if (checked) {
-                {
-                    Icon(
-                        imageVector = Icons.Filled.Check,
-                        contentDescription = null,
-                        modifier = Modifier.size(SwitchDefaults.IconSize),
-                    )
+
+
+            Switch(
+                checked = checked,
+                onCheckedChange = {
+                    onCheckedChange(it)
+
+                },
+                thumbContent = if (checked) {
+                    {
+                        Icon(
+                            imageVector = Icons.Filled.Check,
+                            contentDescription = null,
+                            modifier = Modifier.size(SwitchDefaults.IconSize),
+                        )
+                    }
+                } else {
+                    null
                 }
-            } else {
-                null
-            }
-        )
+            )
+
+
+        }
+    }
+
+
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    fun alarmEveryHour(isOn: Boolean, overrideSilent: Boolean){
+
+        val context: Context = applicationContext
+        AlarmReceiver.scheduleNextAlarm(context, isOn, overrideSilent)
 
 
     }
-}
 
 
 
@@ -100,10 +137,65 @@ fun OnOffToggle(toggleDescription: String, toggleFunction: () -> Unit){
 fun Preview(){
     Column {
         AppHeader(titleResId = R.string.app_name) { }
-        OnOffToggle("Hourly chime") { } }
-        OnOffToggle("Override silent mode") { }
+        OnOffToggle("Hourly chime", true){}
+        OnOffToggle("Override silent mode", true){}
     }
 
 
 }
+}
+
+class AlarmReceiver : BroadcastReceiver() {
+    companion object{
+        @RequiresApi(Build.VERSION_CODES.S)
+        fun scheduleNextAlarm(context: Context, isOn: Boolean, overrideSilent: Boolean) {
+            val alarmManager = context.getSystemService(AlarmManager::class.java)
+
+            val intent = Intent(context, AlarmReceiver::class.java)
+            val pendingIntent =
+                PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+            val calendar = Calendar.getInstance()
+            intent.putExtra("isOn", isOn)
+            intent.putExtra("overrideSilent", overrideSilent)
+            calendar.set(Calendar.MINUTE, 0)
+            calendar.set(Calendar.SECOND, 0)
+            calendar.set(Calendar.MILLISECOND, 0)
+            calendar.add(Calendar.HOUR_OF_DAY, 1)
+            val timeTillNextAlarm = calendar.timeInMillis
+
+            if (isOn && alarmManager.canScheduleExactAlarms()) alarmManager.setExactAndAllowWhileIdle(
+                AlarmManager.RTC_WAKEUP,
+                timeTillNextAlarm,
+                pendingIntent
+            )
+            else alarmManager.cancel(pendingIntent)
+        }
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onReceive(context: Context, intent: Intent) {
+        val isOverrideSilent = intent.getBooleanExtra("overrideSilent", false)
+        val isOn = intent.getBooleanExtra("isOn", true)
+        val mediaPlayer = MediaPlayer.create(context, R.raw.clock_chime_88027)
+        val usage =
+            if (isOverrideSilent) AudioAttributes.USAGE_ALARM else AudioAttributes.USAGE_MEDIA
+        val attributes = AudioAttributes.Builder()
+            .setUsage(usage)
+            .build()
+
+        mediaPlayer.apply{
+            setAudioAttributes(attributes)
+            mediaPlayer.start()
+        }
+        scheduleNextAlarm(context, isOn, isOverrideSilent)
+
+
+
+
+    }
+
+}
+
+
 
